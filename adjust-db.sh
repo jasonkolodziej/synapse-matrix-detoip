f0f8acbd5e51c2b10dac7490757cdb347a52d585
Jason Kolodziej <me@jasonkolodziej.com>
2020-05-02 20:40:11 -0500

    edit homeserver.yaml

---
+++ adjust-db.sh
@@ -0,0 +1,24 @@
+_file=homeserver.yaml
+_db_user_name=matrix_synapse
+echo "Adjusting the DB tag in './files/homeserver.yaml'..."
+echo
+echo "Adding to $_file file..."
+source '.env'
+{
+cat <<-EOF >> ${PWD}/files/homeserver.yaml
+
+database:
+    name: psycopg2
+    args:
+        user: $_db_user_name
+        password: ${POSTGRES_PASSWORD}
+        database: $_db_user_name
+        host: db
+        cp_min: 5
+        cp_max: 10
+
+EOF
+}&> /dev/null
+
+echo
+echo "Done. Please check..."
